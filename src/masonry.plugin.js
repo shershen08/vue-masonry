788756e1a30683c801bab9bac5e62b6541b990a2
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-02-18 18:11:48 +0100

    added eslint

---
+++ src/masonry.plugin.js
@@ -0,0 +1,60 @@
+ import Vue from 'vue'
+ const attributesMap = {
+   'column-width': 'columnWidth',
+   'transition-duration': 'transitionDuration',
+   'item-selector': 'itemSelector',
+   'origin-left': 'originLeft',
+   'origin-top': 'originTop'
+ }
+ const EVENT_ADD = 'vuemasonry.itemAdded'
+ const EVENT_REMOVE = 'vuemasonry.itemRemoved'
+
+ const stringToBool = (val) => (val + '').toLowerCase() === 'true'
+
+ const collectOptions = (attrs) => {
+   let res = {}
+   let attrsrray = Array.prototype.slice.call(attrs)
+   attrsrray.forEach(function (attr) {
+     if (Object.keys(attributesMap).indexOf(attr.name) > -1) {
+       res[ attributesMap[ attr.name ] ] = (attr.name.indexOf('origin') > -1) ? stringToBool(attr.value) : attr.value
+     }
+   })
+   return res
+ }
+
+ var Events = new Vue({})
+
+ export const masonry = Vue.directive('masonry', {
+   props: [ 'transitionDuration', ' itemSelector' ],
+
+   inserted: function (el, nodeObj) {
+     if (!Masonry) {
+       throw new Error('Masonry plugin is not defined. Please check it\'s connected and parsed correctly.')
+     }
+     var masonry = new Masonry(el, collectOptions(el.attributes))
+     var masonryDraw = () => {
+       masonry.reloadItems()
+       masonry.layout()
+     }
+     Vue.nextTick(function () {
+       masonryDraw()
+     })
+
+     Events.$on(EVENT_ADD, function (eventData) {
+       masonryDraw()
+     })
+     Events.$on(EVENT_REMOVE, function (eventData) {
+       masonryDraw()
+     })
+   }
+ })
+
+ export const masonryTile = Vue.directive('masonryTile', {
+
+   inserted: function (el) {
+     Events.$emit(EVENT_ADD, { 'element': el })
+   },
+   beforeDestroy: function (el) {
+     Events.$emit(EVENT_REMOVE, { 'element': el })
+   }
+ })
