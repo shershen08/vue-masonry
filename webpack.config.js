d9fa9149b467b426f2c3f76f17e225ecdbbaaefa
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-08-09 16:32:59 +0200

    add webpack builder for prod

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const UglifyEsPlugin = require('uglify-es-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+     entry: './index.js',
+          output: {
+         path: __dirname,
+         filename: 'dist/vue-masonry-plugin.js',
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         }]
+     },
+    plugins: [
+        new UglifyEsPlugin(),
+        new webpack.BannerPlugin({
+            banner: "Vue.js plugin for Masonry layouts \n https://github.com/shershen08/vue-masonry/ \n file:[file]"
+        })
+    ]
+ }
