ebb4f3fb6d34bae3a7a8b929dc2a2fd2c3f041e7
mkuz <michailkuznetsov@gmail.com>
2017-08-10 23:39:53 +0200

    add es2015 setting to webpack, bump version

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
          loaders: [{
              test: /\.js$/,
              exclude: /node_modules/,
-             loader: 'babel-loader'
+             loader: 'babel-loader',
+              query: {
+                presets: ['es2015']
+            }
          }]
      },
     plugins: [
