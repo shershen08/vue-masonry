cfdbda606315eb3663ea33b14d18cd9f501bcac0
Crutchcorn <crutchcorn@gmail.com>
2018-10-26 17:47:01 -0700

    Update dependancies

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const UglifyEsPlugin = require('uglify-es-webpack-plugin');
-const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack')
 
-const libraryName = 'vue-masonry-plugin';
-const buildTarget = process.env.TARGET === 'umd' ? 'umd' : 'window';
-const outputFile = `${libraryName}-${buildTarget}.js`;
+const libraryName = 'vue-masonry-plugin'
+const buildTarget = process.env.TARGET === 'umd' ? 'umd' : 'window'
+const outputFile = `${libraryName}-${buildTarget}.js`
 
 module.exports = {
-     entry: './index.js',
-          output: {
-         path: __dirname,
-         filename: 'dist/vue-masonry-plugin.js',
-     },
-     module: {
-         loaders: [{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader',
-              query: {
-                presets: ['es2015']
-            }
-         }]
-     },
-     output: {
-        path: __dirname + '/dist',
-        filename: outputFile,
-        library: libraryName,
-        libraryTarget: buildTarget,
-        umdNamedDefine: true
-    },
-    plugins: [
-        new UglifyEsPlugin(),
-        new webpack.BannerPlugin({
-            banner: "Vue.js plugin for Masonry layouts \n https://github.com/shershen08/vue-masonry/ \n file:[file]"
-        })
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        }
+      }
     ]
- }
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: buildTarget,
+    umdNamedDefine: true
+  },
+  mode: 'production',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: 'Vue.js plugin for Masonry layouts \n https://github.com/shershen08/vue-masonry/ \n file:[file]'
+    })
+  ]
+}
